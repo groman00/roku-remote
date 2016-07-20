66265b840d0e9ce260cb794b574155c73ff31c13
Roman, Greg <greg.roman@teamaol.com>
2016-07-20 18:05:49 -0400

    added openshift vars

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+http.listen(app.get('port'), app.get('ip'), function(){
   console.log('listening on *:3000');
 });
