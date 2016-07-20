47f20397ec2afdece59d18cb650b915b430b8594
Roman, Greg <greg.roman@teamaol.com>
2016-07-20 16:42:35 -0400

    added ip address validation.  using personal forked version of node-roku.

--- index.js
+++ index.js
@@ -10,27 +10,41 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
-
-/**
- * Need to create a new Roku instance for each connection
- */
-var device = new Roku('http://192.168.1.188:8060/');
-
 io.on('connection', function(socket){
   
   console.log('a user connected');
 
   socket.on('remote button clicked', function(key){
-    
     console.log('key pressed: ' + key);
+    //console.log(socket.device);
 
-    device.press(Roku[key.toUpperCase()]);
+    if(socket.device) {
+      socket.device.press(Roku[key.toUpperCase()]);
+    } else {
+      socket.emit('abort');
+    }
 
-  });  
+  }); 
+
+  socket.on('verify ip', function(ip){
+    console.log('verifying roku at ' + ip);
+    var device = new Roku('http://' + ip + ':8060/');
+    device.info(function(error, info){
+      if(error) {
+        console.log('error', error);
+        socket.emit('ip failed');
+      } else {
+        console.log(ip + ' verified');
+        socket.device = device;
+        socket.emit('ip verified', info);
+      }
+    });
+  }); 
+
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 var $ = require('jquery');
 var socket = require('socket.io-client')();
+
+
+/**
+ * Remote Event Handling
+ * todo: make this a module
+ */
 var $remote = $('#remote');
 
 $remote.on('click', '.button', function(e){
@@ -11,6 +17,45 @@ $remote.on('click', '.button', function(e){
 });
 
 
+/**
+ * Ip Address form handling
+ * todo: make this a module
+ */
 
+var $overlay = $('#ipOverlay');
+var $form = $('#ipAddressForm');
+var $input = $('#ipAddress');
+
+function toggleFormError(bool){
+    $form.toggleClass('has-error', bool);
+};
+
+$form.on('submit', function(e){
+    var value = $input.val().trim();
+
+    toggleFormError(false);
+
+    if(value > '') {
+        socket.emit('verify ip', value);
+    }else{
+        toggleFormError(true);
+    }
+
+    return false;
+});
+
+socket.on('ip failed', function () {
+    toggleFormError(true);
+});
+
+socket.on('ip verified', function (info) {
+    $overlay.removeClass('in');
+});
+
+socket.on('abort', function (info) {
+    window.location.reload();
+});
 
       
+
+
